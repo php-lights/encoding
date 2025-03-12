f52aa5777efadf91736119b9366c5d2efc68bce7
Samantha Nguyen <contact@samanthanguyen.me>
2025-03-12 15:00:46 -0500

    fix: fix TextCodec return type, add annotations

--- src/TextCodec.php
+++ src/TextCodec.php
@@ -2,6 +2,9 @@
 
 namespace Neoncitylights\Encoding;
 
+/**
+ * @see https://encoding.spec.whatwg.org/#handler
+ */
 interface TextCodec {
-	public function handler( Queue $queue, int $byteOrCodepoint ): HandleStateValue;
+	public function handler( Queue $queue, int $byteOrCodepoint ): HandleStateResult;
 }
