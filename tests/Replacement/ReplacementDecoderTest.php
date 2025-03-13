b0e1a54d406945ee58bf2e53bffffd45c0f1319a
Samantha <contact@samanthanguyen.me>
2025-03-13 14:05:26 -0500

    feat: implement ReplacementDecoder (#15)

---
+++ tests/Replacement/ReplacementDecoderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Neoncitylights\Encoding\Replacement\Tests;
+
+use Neoncitylights\Encoding\HandleState;
+use Neoncitylights\Encoding\HandleStateResult;
+use Neoncitylights\Encoding\Queue;
+use Neoncitylights\Encoding\Replacement\ReplacementDecoder;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass( ReplacementDecoder::class )]
+#[UsesClass( HandleState::class )]
+#[UsesClass( HandleStateResult::class )]
+#[UsesClass( Queue::class )]
+class ReplacementDecoderTest extends TestCase {
+	public function testEndOfQueueEarlyFinished() {
+		$decoder = new ReplacementDecoder();
+		$queue = Queue::newFromArray( [ 1, 2, 3 ] );
+
+		$result = $decoder->handler( $queue, 3 );
+		$this->assertTrue( $result->state->isFinished() );
+	}
+
+	public function testError() {
+		$decoder = new ReplacementDecoder();
+		$queue = Queue::newFromArray( [ 1, 2, 3 ] );
+
+		$result = $decoder->handler( $queue, 4 );
+		$this->assertTrue( $result->state->isError() );
+	}
+
+	public function testFinishedAfterError() {
+		$decoder = new ReplacementDecoder();
+		$queue = Queue::newFromArray( [ 1, 2, 3 ] );
+
+		$result = $decoder->handler( $queue, 4 );
+		$result = $decoder->handler( $queue, 4 );
+		$this->assertTrue( $result->state->isFinished() );
+	}
+}
